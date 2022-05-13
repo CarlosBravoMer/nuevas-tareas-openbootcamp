8377130e37461f6d1d10a30b3170ed66f1ca04b3
CB-BLACK <102660320+CB-BLACK@users.noreply.github.com>
2022-05-13 11:37:23 +0200

    Add files via upload

---
+++ tarea-sesion02/index.js
@@ -0,0 +1,8 @@
+const libro = {
+  titulo: "100 años de soledad",
+  autor: "Gabriel Garcia Marquez",
+  fecha: new Date("3-5-1967"),
+  url: "https://es.wikipedia.org/wiki/Cien_a%C3%B1os_de_soledad",
+};
+
+let persona = ["Carlos", 27, true, new Date("6-24-1994"), libro];
