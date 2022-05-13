e82eeaf8c54476d1aa3ab7c717dd4a2c3edef63f
CB-BLACK <102660320+CB-BLACK@users.noreply.github.com>
2022-05-13 15:37:37 +0200

    Add files via upload

---
+++ tarea-sesion05/index.js
@@ -0,0 +1,17 @@
+// - Una variable que contenga tu altura en centímetros (entero)
+let altura = 195;
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+let altura2 = 1.95;
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = 110.95;
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaRedondeada = Math.round(altura2);
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoRedondeado = Math.floor(peso);
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+const igualdad = Number.MAX_VALUE + 1 === Number.MAX_VALUE
